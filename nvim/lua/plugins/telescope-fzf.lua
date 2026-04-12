c166da6e7bcb19f4374064b091d6afcde399759e
Bluedog <139224044+deadpanpanda@users.noreply.github.com>
2026-04-13 08:21:21 +1000

    Update telescope

--- nvim/lua/plugins/telescope-fzf.lua
+++ nvim/lua/plugins/telescope-fzf.lua
@@ -7,6 +7,14 @@ return {
     },
   },
   opts = function(_, opts)
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+      file_ignore_patterns = { "%.git/" },
+    })
+    opts.pickers = vim.tbl_deep_extend("force", opts.pickers or {}, {
+      find_files = {
+        hidden = true,
+      },
+    })
     local ok, _ = pcall(require, "telescope")
     if ok then
       require("telescope").load_extension("fzf")
